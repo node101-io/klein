4a0029ac78312865ba7278ef80af53ae3c8103eb
aytunc <109796545+saitunc@users.noreply.github.com>
2023-02-24 19:10:57 +0300

    D

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,77 +9,76 @@ use std::io::{Read};
 // use std::io::{Read, Write};
 use tauri::{PhysicalSize, Manager};
 
+struct SessionManager{
+    open_session: Session,
+}
+
+static mut GLOBAL_STRUCT: Option<Box<SessionManager>> = None;
+
+fn create_session(session: Session) -> Box<SessionManager>{
+    let mut alive_session = SessionManager{
+        open_session:session,
+    };  
+    Box::new(alive_session)
+}
+
+fn modify_session(session:Session,manager: &mut SessionManager) {
+    manager.open_session = session;
+}
+
+
+#[tauri::command]
+async fn log_in(ip: String, password: String, window: tauri::Window) -> () {
+    let tcp = std::net::TcpStream::connect(format!("{}:22", ip)).unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    let res = sess.userauth_password("root", &password);
+    println!("arrived here,");
+    println!("{}",res.is_ok());
+
+    if res.is_ok(){
+        unsafe{
+        GLOBAL_STRUCT = Some(create_session(sess));
+        let mut x = (GLOBAL_STRUCT.as_ref().unwrap()).open_session.channel_session().unwrap();
+
+        x.exec("ls").unwrap();
+        let mut s = String::new();
+        x.read_to_string(&mut s).unwrap();
+        print!("{}", s);
+    }
+        window.eval("window['loadNewPage']('mainpage/mainpage')").unwrap();
+    }
+    else{
+        // Javascript hata bastıracak.
+        // window.eval("window['login_error']").unwrap();
+    }
+
+}
+
+#[tauri::command]
+fn did_i_logged_in(){
+    unsafe{
+    println!("YO I AM HERE");
+    if let Some(my_boxed_session) = GLOBAL_STRUCT.as_ref(){
+    let mut channel = (*my_boxed_session).open_session.channel_session().unwrap();
+    channel.exec("ls").unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s).unwrap();
+    print!("{}", s);   
+    }
+}
+}
+
+
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
             app.get_window("main").unwrap().set_min_size(Some(PhysicalSize::new(1280, 720))).unwrap();
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![log_in])
+        .invoke_handler(tauri::generate_handler![log_in,did_i_logged_in])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
-#[tauri::command]
-async fn log_in(ip: String, password: String, remember: bool, window: tauri::Window) -> () {
-    let tcp = std::net::TcpStream::connect(format!("{}:22", ip)).unwrap();
-    let mut sess = Session::new().unwrap();
-    sess.set_tcp_stream(tcp);
-    sess.handshake().unwrap();
-    sess.userauth_password("root", &password).unwrap();
-
-    let mut channel = sess.channel_session().unwrap();
-    channel.exec("ls").unwrap();
-    let mut s = String::new();
-    channel.read_to_string(&mut s).unwrap();
-    print!("{}", s);
-    if remember {
-        println!("Remembering password");
-    }
-
-    window.eval("window['loadNewPage']('mainpage/mainpage')").unwrap();
-    // window.eval("window['showLoginError']()").unwrap(); //HATA MESAJINI GÖSTERMEK İÇİN
-}
-
-// #[tauri::command]
-// async fn log_in(ip: String, password: String, window: tauri::Window) -> Result<(), String> {
-//     let tcp = match std::net::TcpStream::connect(format!("{}:22", ip)) {
-//         Ok(tcp) => tcp,
-//         Err(e) => return Err(format!("Failed to connect to {}: {}", ip, e)),
-//     };
-    
-//     let mut sess = match Session::new() {
-//         Ok(sess) => sess,
-//         Err(e) => return Err(format!("Failed to create SSH session: {}", e)),
-//     };
-    
-//     sess.set_tcp_stream(tcp);
-    
-//     if let Err(e) = sess.handshake() {
-//         return Err(format!("Failed to perform SSH handshake: {}", e));
-//     }
-    
-//     if let Err(e) = sess.userauth_password("root", &password) {
-//         return Err(format!("Failed to authenticate with remote machine: {}", e));
-//     }
-    
-//     let mut channel = match sess.channel_session() {
-//         Ok(channel) => channel,
-//         Err(e) => return Err(format!("Failed to open SSH channel: {}", e)),
-//     };
-    
-//     if let Err(e) = channel.exec("ls") {
-//         return Err(format!("Failed to execute command on remote machine: {}", e));
-//     }
-    
-//     let mut s = String::new();
-//     if let Err(e) = channel.read_to_string(&mut s) {
-//         return Err(format!("Failed to read output from remote machine: {}", e));
-//     }
-    
-//     if let Err(e) = window.eval("window['loadNewPage']('newpage')") {
-//         return Err(format!("Failed to execute JavaScript on Tauri window: {}", e));
-//     }
-    
-//     Ok(())
-// }
