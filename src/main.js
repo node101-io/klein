5f180a9b5aab77097766ea49f444947a077ea483
Necip Baha Sağıroğlu <sagiroglun22@itu.edu.tr>
2023-02-27 16:14:17 +0300

    mainpage taslak

--- src/main.js
+++ src/main.js
@@ -1,24 +1,37 @@
 const { invoke } = window.__TAURI__.tauri;
 const { readTextFile, writeFile } = window.__TAURI__.fs;
+const ARROWHEAD_UP = "assets/arrowhead-up.png";
+const ARROWHEAD_DOWN = "assets/arrowhead-down.png";
+const EYE_ON = "assets/eye-on.png";
+const EYE_OFF = "assets/eye-off.png";
+let focusedIndex;
 let ipAddresses;
 
-// Disable right click
+// Disable right click:
 // document.addEventListener("contextmenu", event => event.preventDefault());
 
 readTextFile("ipaddresses.json").then((data) => {
   ipAddresses = JSON.parse(data);
 });
 
-function passwordVisibility(passwordInputEl) {
-  passwordInputEl.type = passwordInputEl.type == "password" ? "text" : "password";
-  document.getElementById("visibility-toggle").src = passwordInputEl.type == "password" ? "assets/eye-off.png" : "assets/eye-on.png";
-}
-
-function showLoginError() {
-  document.querySelector(".login-page-warning").style.removeProperty("display");
-}
+// Functions to be called from Rust
 function loadNewPage(pagename) {
   window.location.href = pagename;
+}
+function showLoginError() {
+  hideLoadingAnimation();
+  document.querySelector(".login-page-warning").style.removeProperty("display");
+}
+
+function showLoadingAnimation() {
+  document.querySelector(".all-wrapper").style.setProperty("pointer-events", "none");
+  document.querySelector(".all-wrapper").style.setProperty("display", "none");
+  document.querySelector(".boxes").style.setProperty("display", "unset");
+}
+function hideLoadingAnimation() {
+  document.querySelector(".boxes").style.setProperty("display", "none");
+  document.querySelector(".all-wrapper").style.removeProperty("display");
+  document.querySelector(".all-wrapper").style.removeProperty("pointer-events");
 }
 
 function showSelectedItem(ip, icon) {
@@ -32,6 +45,15 @@ function showSelectedItem(ip, icon) {
   selectedItemEl.children[1].textContent = icon;
   selectedItemEl.children[2].textContent = ip;
 }
+function highlightItem(x) {
+  if (x.length > 1) {
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+    focusedIndex = (focusedIndex + x.length - 2) % (x.length - 1) + 1;
+    x[focusedIndex].classList.add("autocomplete-active");
+  }
+}
 
 window.addEventListener("DOMContentLoaded", () => {
   let ipInputEl = document.getElementById("ip-input");
@@ -43,17 +65,18 @@ window.addEventListener("DOMContentLoaded", () => {
   let loginButtonEl = document.getElementById("login-button");
   let checkboxInputEl = document.getElementById("checkbox-input");
 
-  new MutationObserver(() => { dropdownToggleEl.src = (autocompleteListEl.innerText == "") ? "assets/down-arrowhead.png" : "assets/up-arrowhead.png"; }).observe(autocompleteListEl, { childList: true });
+  dropdownToggleEl.src = ARROWHEAD_DOWN;
+  new MutationObserver(() => { dropdownToggleEl.src = (autocompleteListEl.innerHTML == "") ? ARROWHEAD_DOWN : ARROWHEAD_UP; }).observe(autocompleteListEl, { childList: true });
   
   ipInputEl.addEventListener("keydown", function(e) {
     x = document.querySelectorAll(".each-autocomplete-item");
     if (e.keyCode == 40) {
       focusedIndex++;
-      addActive(x);
+      highlightItem(x);
     }
     else if (e.keyCode == 38) {
       focusedIndex--;
-      addActive(x);
+      highlightItem(x);
     }
     else if (e.keyCode == 13 && focusedIndex > -1) {
       x[focusedIndex].click();
@@ -70,18 +93,14 @@ window.addEventListener("DOMContentLoaded", () => {
         div.addEventListener("click", function() {
           showSelectedItem.call(this, ipAddresses[i].ip, ipAddresses[i].icon);
         });
-
         img = document.createElement("img");
         img.setAttribute("class", "each-autocomplete-item-icon");
         img.setAttribute("src", `assets/${ipAddresses[i].icon.toLowerCase()}.png`);
-
         div1 = document.createElement("div");
         div1.textContent = ipAddresses[i].icon;
-
         div2 = document.createElement("div");
         div2.setAttribute("class", "each-autocomplete-item-ip");
         div2.textContent = ipAddresses[i].ip;
-
         div.appendChild(img);
         div.appendChild(div1);
         div.appendChild(div2);
@@ -96,39 +115,28 @@ window.addEventListener("DOMContentLoaded", () => {
     ipInputEl.focus();
   });
 
+  visibilityToggleEl.src = EYE_OFF;
   visibilityToggleEl.addEventListener("click", function() {
       passwordInputEl.type = passwordInputEl.type == "password" ? "text" : "password";
-      visibilityToggleEl.src = passwordInputEl.type == "password" ? "assets/eye-off.png" : "assets/eye-on.png";
+      visibilityToggleEl.src = passwordInputEl.type == "password" ? EYE_OFF : EYE_ON;
   });
 
   loginButtonEl.addEventListener("click", function() {
-    invoke("log_in", { ip: ipInputEl.value, password: passwordInputEl.value, remember: checkboxInputEl.checked });
+    if (ipInputEl.value == "" || passwordInputEl.value == "") {
+      showLoginError();
+    }
+    else {
+      showLoadingAnimation();
+      invoke("log_in", { ip: ipInputEl.value, password: passwordInputEl.value, remember: checkboxInputEl.checked });
+    }
   });
 
-  function addActive(x) {
-    if (x == undefined) return false;
-    for (var i = 0; i < x.length; i++) {
-      x[i].classList.remove("autocomplete-active");
-    }
-    if (focusedIndex >= x.length) focusedIndex = 1;
-    else if (focusedIndex < 1) focusedIndex = (x.length - 1);
-
-    x[focusedIndex].classList.add("autocomplete-active");
-  }
-
   document.addEventListener("click", function (e) {
-    switch (e.target) {
-      case dropdownToggleEl:
-        if (dropdownToggleEl.src.includes("up-arrowhead")) {
-          autocompleteListEl.innerHTML = "";
-        }
-        else {
-          ipInputEl.dispatchEvent(new Event("input", { "bubbles": true, "cancelable": true }));
-        }
-        break;
-      default:
-        autocompleteListEl.innerHTML = "";
-        break;
+    if (e.target == dropdownToggleEl && !dropdownToggleEl.src.includes(ARROWHEAD_UP)) {
+      ipInputEl.dispatchEvent(new Event("input", { bubbles: true }));
+    }
+    else {
+      autocompleteListEl.innerHTML = "";
     }
   });
 });
--- src/mainpage/mainpage.js
+++ src/mainpage/mainpage.js
@@ -1,5 +1,18 @@
 const { invoke } = window.__TAURI__.tauri;
 
-function did_i_logged_in() {
-    invoke("did_i_logged_in");
-}
+window.addEventListener("DOMContentLoaded", () => {
+  let testnetTabButton = document.getElementById("testnet-tab-button");
+  let mainnetTabButton = document.getElementById("mainnet-tab-button");
+
+  testnetTabButton.addEventListener("click", () => {
+    testnetTabButton.setAttribute("class", "each-nodes-page-tab active-tab");
+    mainnetTabButton.setAttribute("class", "each-nodes-page-tab");
+    // sayfa içeriğini değiştir
+  });
+
+  mainnetTabButton.addEventListener("click", () => {
+    testnetTabButton.setAttribute("class", "each-nodes-page-tab");
+    mainnetTabButton.setAttribute("class", "each-nodes-page-tab active-tab");
+    // sayfa içeriğini değiştir
+  });
+});
