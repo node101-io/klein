7e64465f23b83c9f857730cbdf6ee490c2afa90c
Berkay GÜNDÜZ <gunduzberkay1532@gmail.com>
2023-02-27 14:11:44 +0300

    "page3" html css js files are editted

--- src/Manage-Node/page3.js
+++ src/Manage-Node/page3.js
@@ -1,22 +1,42 @@
 // Functions
 function toggleVisibilityOfValidatorOpeation() {
-let subButtons = document.getElementById("sub-buttons");
-if (subButtons.style.display === "none") {
-    subButtons.style.display = "block";
-} else {
-    subButtons.style.display = "none";
-}
-}
-function copyWalletAddress() {
-var button = document.getElementsByClassName("wallet-address");
-var text = button.innerHTML;
+    let subButtons = document.getElementById("subbuttons-ul");
+    
+    style = window.getComputedStyle(subButtons)
+    visibility = style.getPropertyValue("display")
 
-navigator.clipboard
-    .writeText(text)
-    .then(function () {
-    console.log("Text copied to clipboard");
-    })
-    .catch(function (err) {
-    console.error("Error copying text: ", err);
-    });
+    if (visibility === "none") {
+        subButtons.style.display = "block";
+    } else {
+        subButtons.style.display = "none";
+    }
 }
+
+function copyWalletAddress() {
+    let button = document.getElementsByClassName("wallet-address");
+    console.log(button)
+    navigator.clipboard
+        .writeText(button[0].innerText)
+        .then(function () {
+        console.log("Text copied to clipboard");
+        })
+        .catch(function (err) {
+        console.error("Error copying text: ", err);
+        });
+}
+
+
+
+
+
+
+document.getElementById('validator-operations').addEventListener('click', function() {
+    toggleVisibilityOfValidatorOpeation()
+});
+document.getElementsByClassName('wallet-address'.addEventListener('click', function () {
+    copyWalletAddress()
+}))
+document.getElementsByClassName('fa-solid fa-copy'.addEventListener('click', function () {
+    copyWalletAddress()
+}))
+
