2c6c1773cbd96d49d6cf28a2c49b18a315a5c78c
Berkay GÜNDÜZ <gunduzberkay1532@gmail.com>
2023-02-28 14:07:54 +0300

    manage node page content side is done

--- src/Manage-Node/page3.js
+++ src/Manage-Node/page3.js
@@ -1,7 +1,7 @@
 // Functions
 function toggleVisibilityOfValidatorOpeation() {
     let subButtons = document.getElementById("subbuttons-ul");
-    
+
     style = window.getComputedStyle(subButtons)
     visibility = style.getPropertyValue("display")
 
@@ -11,7 +11,6 @@ function toggleVisibilityOfValidatorOpeation() {
         subButtons.style.display = "none";
     }
 }
-
 function copyWalletAddress() {
     let button = document.getElementsByClassName("wallet-address");
     console.log(button)
@@ -24,19 +23,38 @@ function copyWalletAddress() {
         console.error("Error copying text: ", err);
         });
 }
-
-
-
-
+function setNewStyleForDeleteButton() {
+    let deleteButton = document.getElementsByClassName('node-delete-button')[0];
+    let deleteButtonIcon = document.getElementsByClassName('delete-icon')[0];
+    console.log(deleteButton);
+    console.log(deleteButtonIcon);
+    deleteButton.style.color = "white";
+    deleteButton.style.background = "red";
+    deleteButtonIcon.style.color = "white";
+}
+function setOldStyleForDeleteButton() {
+    let deleteButton = document.getElementsByClassName('node-delete-button')[0];
+    let deleteButtonIcon = document.getElementsByClassName('delete-icon')[0];
+    console.log(deleteButton);
+    console.log(deleteButtonIcon);
+    deleteButton.style.color = "red";
+    deleteButton.style.background = "transparent";
+    deleteButtonIcon.style.color = "red";
+}
 
 
 document.getElementById('validator-operations').addEventListener('click', function() {
     toggleVisibilityOfValidatorOpeation()
-});
-document.getElementsByClassName('wallet-address'.addEventListener('click', function () {
+})
+document.getElementsByClassName('wallet-address')[0].addEventListener('click', function () {
     copyWalletAddress()
-}))
-document.getElementsByClassName('fa-solid fa-copy'.addEventListener('click', function () {
+})
+document.getElementsByClassName('fa-solid fa-copy')[0].addEventListener('click', function () {
     copyWalletAddress()
-}))
-
+})
+document.getElementsByClassName('node-delete-button')[0].addEventListener('mouseover', function () {
+    setNewStyleForDeleteButton()
+})
+document.getElementsByClassName('node-delete-button')[0].addEventListener('mouseout', function () {
+    setOldStyleForDeleteButton()
+})
