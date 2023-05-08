ed430b1f0769e2d77bddfca497fdf0ea93a9888f
Necip <sagiroglun22@itu.edu.tr>
2023-05-08 12:54:06 +0300

    updater fix

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 const { tauri, dialog, clipboard, http, event: tevent } = window.__TAURI__;
 
-// setTimeout(async () => {
-//   await updater.checkUpdate().catch(e => console.log(e));
-// }, 2000);
 
 // for development purposes
 // localStorage.setItem("ipaddresses", '[{"ip":"144.91.93.154","icon":"Celestia Light","validator_addr":"celestia1087ym92534gj8jlwjqaa98mh6hqfgyzgmlwdct"},{"ip":"213.136.73.17","icon":"Nibiru","validator_addr":"nibi1y2kza3dstaqhk87p9yel5xyksa8atddmz6n63m"}]');
@@ -15,27 +12,26 @@ const ipAddresses = localStorage.getItem("ipaddresses") ? JSON.parse(localStorag
 
 const fetchProjects = async () => {
   const client = await http.getClient();
-  const authenticate = await client.post('https://admin.node101.io/api/authenticate', {
-    type: 'Json',
+  const authenticate = await client.post("https://admin.node101.io/api/authenticate", {
+    type: "Json",
     payload: { key: "b8737b4ca31571d769506c4373f5c476e0a022bf58d5e0595c0e37eabb881ad150b8c447f58d5f80f6ffe5ced6f21fe0502c12cf32ab33c6f0787aea5ccff153" },
   });
 
   projects.length = 0;
   for (let count = 0; ; count++) {
     projects_data = await client.get(`https://admin.node101.io/api/testnets?page=${count}`, {
-      type: 'Json',
+      type: "Json",
       headers: {
-        'Cookie': authenticate.headers['set-cookie']
+        "Cookie": authenticate.headers["set-cookie"]
       }
     })
     if (!projects_data.data.testnets.length) break;
     projects.push(...projects_data.data.testnets);
   }
-  console.log(projects);
 }
 
-// window.addEventListener('contextmenu', (e) => {
-//   if (e.target.tagName === 'INPUT' && e.target.type == "text") return;
+// window.addEventListener("contextmenu", (e) => {
+//   if (e.target.tagName === "INPUT" && e.target.type == "text") return;
 //   e.preventDefault();
 // });
 
